bf9fb95e6a79803c0749321f46e6a3061e342b75
Sara Wardina <66263880+sarawrdn@users.noreply.github.com>
2023-04-17 12:18:03 +0800

    Update signaturePad.blade.php

--- resources/views/signaturePad.blade.php
+++ resources/views/signaturePad.blade.php
@@ -1,6 +1,4 @@
-@extends('user.sidebar')
 
-@section('body')
 <head>
     <title>Laravel Signature Pad Tutorial Example - ItSolutionStuff.com </title>
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.css">
@@ -61,5 +59,4 @@
         $("#signature64").val('');
     });
 </script>
-</body>
-</html>
+
